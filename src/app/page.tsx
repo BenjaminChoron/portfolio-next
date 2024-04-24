316b4eebf06527d3b625e19d09c6afcac1bb615e
BenjCH <benjamin.choron@mediflash.fr>
2024-04-24 10:35:07 +0200

    :sparkles: Add desktop and mobile navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen p-5 sm:p-10 sm:pt-32 bg-primary text-secondary">
       <h1 className="text-xl">Hello World!</h1>
     </main>
   );
