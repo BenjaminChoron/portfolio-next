b70557c9cfed40ed4e6fda4b7f14dbf1d81b9416
BenjCH <benjamin.choron@mediflash.fr>
2024-04-24 09:45:30 +0200

    :lipstick: Add DaisyUI themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,28 @@ const config: Config = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#FFEED9',
+          secondary: '#233C44',
+          accent: '#BDA88D',
+        },
+        dark: {
+          primary: '#233C44',
+          secondary: '#BDA88D',
+          accent: '#FFEED9',
+        },
+      },
+    ],
+    darkTheme: 'dark', // Whether to use dark mode by default
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ':root', // The element that receives theme color CSS variables
+  },
 };
 export default config;
